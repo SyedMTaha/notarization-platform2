b1e9bf691d0654d2024b46edf7e4f0cab83f4187
DaaimAliSheikh <daaimalisheikh23@gmail.com>
2025-03-26 16:04:45 +0500

    imported sass files in dashboard layout(font issue still persists)

--- app/[locale]/dashboard/layout.js
+++ app/[locale]/dashboard/layout.js
@@ -3,6 +3,10 @@ import { LayoutProvider } from "@/context/useLayoutContext";
 import VerticalLayout from "@/layout/VerticalLayout";
 import React from "react";
 
+
+// import "../../../public/assets/scss/Default.scss";
+// import "../../../public/assets/scss/Icons.scss";
+
 function layout({ children }) {
   return (
     <LayoutProvider>
